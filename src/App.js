38630f893e044d96758b9c1b75bfbb40fd68f178
jiradejDev <jiradej@sobtid.me>
2020-10-10 18:13:08 +0700

    fix: error alert

--- src/App.js
+++ src/App.js
@@ -24,20 +24,20 @@ class App extends Component {
 
   }
 
-  getResumeData(){
-    $.ajax({
-      url:'/resumeData.json',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
+  // getResumeData(){
+  //   $.ajax({
+  //     url:'/resumeData.json',
+  //     dataType:'json',
+  //     cache: false,
+  //     success: function(data){
+  //       this.setState({resumeData: data});
+  //     }.bind(this),
+  //     error: function(xhr, status, err){
+  //       console.log(err);
+  //       alert(err);
+  //     }
+  //   });
+  // }
  
   componentDidMount(){
     this.getResumeData();
